12c3faadafe079cf8f6021c310c5a6b99c87264b
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-01-10 12:50:21 +0100

    pokedex methode v1

--- files/class_py/pokedex.py
+++ files/class_py/pokedex.py
@@ -1,3 +1,39 @@
+import json
+import random
+
 class Pokedex:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
+
+    def ouverture_pokemonjson(self):
+        with open('pokemon.json', 'r') as fichier:
+            self.donnees_pokemon = json.load(fichier)
+            return self.donnees_pokemon
+        
+    def information_pokemon(self, data):
+        self.info_pokemon = self.ouverture_pokemonjson()
+        self.pokemon_liste = []
+        for info_pokemon in self.info_pokemon:
+            self.pokemon_liste(info_pokemon[data])  
+            print (info_pokemon[data])
+
+    def rand_pokemon(self):
+        self.info_pokemon = self.ouverture_pokemonjson()
+        self.pokeliste = []
+        for pokemon in self.info_pokemon:
+            self.pokeliste.append(pokemon)
+        self.poke_random = random.choice(self.pokeliste)
+        print (self.poke_random)
+
+    def info_rand_pokemon(self, data):
+        self.info_pokemon = self.ouverture_pokemonjson()
+        for pokemon in self.info_pokemon:
+            if pokemon['nom'] == self.poke_random['nom']:
+                print(pokemon[data])
+
+
+    
+pokedex = Pokedex()
+pokedex.rand_pokemon()
+pokedex.info_rand_pokemon('type')
+pokedex.info_rand_pokemon('debut')
