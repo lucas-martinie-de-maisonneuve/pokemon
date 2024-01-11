3c99882d2bf293693f037bb7048a0a6c3e6c0240
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-01-11 15:07:04 +0100

    in progress

--- files/class_py/maps_combat.py
+++ files/class_py/maps_combat.py
@@ -2,6 +2,7 @@ import pygame
 from files.class_py.screen import Screen
 from files.class_py.element import Element
 from files.class_py.combat import Combat
+from files.class_py.pokedex import Pokedex
 # from files.class_py.pokedex import Pokedex
 import random
 
@@ -13,13 +14,14 @@ class Maps:
         element = Element()
         screen = Screen()
         combat = Combat()
+        pokedex = Pokedex()
         # pokedex = Pokedex()
 
-        # pokemon_liste = ["salameche", "reptincelle", "dracaufeu", "bulbizarre", "herbizarre", "florizarre", "carapuce", "carabaffe", "tortank", "pikachu", "raichu", "chenipotte", "blindalys", "papinox", "canarticho", "racaillou", "gravalanch", "grolem", "evoli", "aquali", "voltali", "pyroli", "phyllali", "tauros", "kangourex", "elektek", "magmar", "scarabrute", "magicarpe", "leviator", "grainipiot", "pifeuil", "tengalice", "roucool", "roucoups", "roucarnage", "goupix", "fenard", "sabelette", "sablaireau", "osselait", "ossatueur", "insecateur", "magneti", "artikodin", "electhor", "sulfura", "kyogre", "groudon", "rayquaza"]
+        pokemon_liste = ["salameche", "reptincelle", "dracaufeu", "bulbizarre", "herbizarre", "florizarre", "carapuce", "carabaffe", "tortank", "pikachu", "raichu", "chenipotte", "blindalys", "papinox", "canarticho", "racaillou", "gravalanch", "grolem", "evoli", "aquali", "voltali", "pyroli", "phyllali", "tauros", "kangourex", "elektek", "magmar", "scarabrute", "magicarpe", "leviator", "grainipiot", "pifeuil", "tengalice", "roucool", "roucoups", "roucarnage", "goupix", "fenard", "sabelette", "sablaireau", "osselait", "ossatueur", "insecateur", "magneti", "artikodin", "electhor", "sulfura", "kyogre", "groudon", "rayquaza"]
         # rand_poke = str(pokedex.info_rand_pokemon(["nom"])).lower()
         # poke_random = pokedex.info_rand_pokemon(rand_poke)
-        # img_poke = random.choice(pokemon_liste)
-        # img_poke2 = random.choice(pokemon_liste)
+        img_poke = random.choice(pokemon_liste)
+        img_poke2 = random.choice(pokemon_liste)
         # screen.img(50, 150, 150, 150, f'pokemon/{img_poke}')
         pokedex.info_rand_pokemon
 
