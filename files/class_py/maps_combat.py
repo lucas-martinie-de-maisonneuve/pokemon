bf23e044308d52eece27faabb9ae6e06b0a11a9c
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-01-11 14:19:59 +0100

    in progress

--- files/class_py/maps_combat.py
+++ files/class_py/maps_combat.py
@@ -1,7 +1,7 @@
 import pygame
 from files.class_py.screen import Screen
 from files.class_py.element import Element
-from files.class_py.pokedex import Pokedex
+# from files.class_py.pokedex import Pokedex
 import random
 
 class Maps:
@@ -11,7 +11,7 @@ class Maps:
     def home(self):
         element = Element()
         screen = Screen()
-        pokedex = Pokedex()
+        # pokedex = Pokedex()
 
         pokemon_liste = ["salameche", "reptincelle", "dracaufeu", "bulbizarre", "herbizarre", "florizarre", "carapuce", "carabaffe", "tortank", "pikachu", "raichu", "chenipotte", "blindalys", "papinox", "canarticho", "racaillou", "gravalanch", "grolem", "evoli", "aquali", "voltali", "pyroli", "phyllali", "tauros", "kangourex", "elektek", "magmar", "scarabrute", "magicarpe", "leviator", "grainipiot", "pifeuil", "tengalice", "roucool", "roucoups", "roucarnage", "goupix", "fenard", "sabelette", "sablaireau", "osselait", "ossatueur", "insecateur", "magneti", "artikodin", "electhor", "sulfura", "kyogre", "groudon", "rayquaza"]
         # rand_poke = str(pokedex.info_rand_pokemon(["nom"])).lower()
