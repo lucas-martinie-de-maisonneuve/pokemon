2571bef6e14db980feebd2cc51a32c3264024ffb
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-01-12 17:56:28 +0100

    color menu combat

--- files/class_py/maps_combat.py
+++ files/class_py/maps_combat.py
@@ -19,7 +19,7 @@ class Maps:
         pokedex = Pokedex()
         # combat = Combat()
 
-        pokemon_liste = ["salameche", "reptincelle", "dracaufeu", "bulbizarre", "herbizarre", "florizarre", "carapuce", "carabaffe", "tortank", "pikachu", "raichu", "chenipotte", "blindalys", "papinox", "canarticho", "racaillou", "gravalanch", "grolem", "evoli", "aquali", "voltali", "pyroli", "phyllali", "tauros", "kangourex", "elektek", "magmar", "scarabrute", "magicarpe", "leviator", "grainipiot", "pifeuil", "tengalice", "roucool", "roucoups", "roucarnage", "goupix", "fenard", "sabelette", "sablaireau", "osselait", "ossatueur", "insecateur", "magneti", "artikodin", "electhor", "sulfura", "kyogre", "groudon", "rayquaza"]
+        self.pokemon_liste = ["salameche", "reptincelle", "dracaufeu", "bulbizarre", "herbizarre", "florizarre", "carapuce", "carabaffe", "tortank", "pikachu", "raichu", "chenipotte", "blindalys", "papinox", "canarticho", "racaillou", "gravalanch", "grolem", "evoli", "aquali", "voltali", "pyroli", "phyllali", "tauros", "kangourex", "elektek", "magmar", "scarabrute", "magicarpe", "leviator", "grainipiot", "pifeuil", "tengalice", "roucool", "roucoups", "roucarnage", "goupix", "fenard", "sabelette", "sablaireau", "osselait", "ossatueur", "insecateur", "magneti", "artikodin", "electhor", "sulfura", "kyogre", "groudon", "rayquaza"]
 
         img_poke = random.choice(self.pokemon_liste)
         img_poke2 = random.choice(self.pokemon_liste)
@@ -51,47 +51,47 @@ class Maps:
                             pokedex.show_pokedex()
                             pokedex.pokedex_run = True
                             
-            element.img(525, 200, 1244, 700,'combat/fight_background')
+            element.img(525, 200, 1244, 700,"combat/fight_background")
             element.button_rect(element.brown,525,650,screen.W,215)            
-            element.img_mir(300, 375, 150, 180, f'pokemon/{img_poke}')                    
+            element.img_mir(300, 375, 150, 180, f"pokemon/{img_poke}")                    
             element.img(660, 225, 150, 180, f"pokemon/{img_poke2}")
             # combat.afficher_capacite()
-            element.img(300, 625, 470, 150, 'combat/background_texte')           
-            element.img(850, 625, 399, 150,'combat/zone_texte')
+            element.img(300, 625, 470, 150, "combat/background_texte")           
+            element.img(850, 625, 399, 150,"combat/zone_texte")
             element.texte(20, "What do you mean ?", element.black, 300, 625)
             # element.rect(screen.W -200,screen.H,1000,200," //")
-            element.button_rect(element.red,760,600,145,45)
-            element.simple_rect(element.black,760,600,125,35,1)
+            element.button_rect(element.darkred,765,600,145,45)
+            element.simple_rect(element.red,765,600,135,35,2)
 
-            element.button_rect(element.blue,940,600,145,45)
-            element.simple_rect(element.black, 940,600,125,35,1)
+            element.button_rect(element.darkblue,945,600,145,45)
+            element.simple_rect(element.blue, 945,600,135,35,2)
 
-            element.button_rect(element.purple,760,650,145,45)
-            element.simple_rect(element.black, 760,650,125,35,1)
+            element.button_rect(element.darkgreen,765,650,145,45)
+            element.simple_rect(element.green, 765,650,135,35,2)
 
-            element.button_rect(element.yellow,940,650,145,45)
-            element.simple_rect(element.black, 940,650,125,35,1)
+            element.button_rect(element.orange,945,650,145,45)
+            element.simple_rect(element.yellow, 945,650,135,35,2)
 
             if self.action == 1 :
-                self.button_attack = element.texte(18, "Attack", element.white, 760, 600)
+                self.button_attack = element.texte(19, "Attack", element.white, 765, 600)
                 element.img(675, 600, 15, 15, f"combat/arrow")
             else:
-                self.button_attack = element.texte(18, "Attack", element.black, 760, 600)
+                self.button_attack = element.texte(18, "Attack", element.black, 765, 600)
             if self.action == 2 :
-                self.button_run = element.texte(18, "Flee", element.white, 940 , 600)
+                self.button_run = element.texte(19, "Flee", element.white, 945 , 600)
                 element.img(855, 600, 15, 15, f"combat/arrow")
             else:
-                self.button_run = element.texte(18, "Flee", element.black, 940 , 600)
+                self.button_run = element.texte(18, "Flee", element.black, 945 , 600)
             if self.action == 3 :
-                self.button_bag = element.texte(18,"Bag", element.white, 760, 650)
+                self.button_bag = element.texte(19,"Bag", element.white, 765, 650)
                 element.img(675, 650, 15, 15, f"combat/arrow")
             else:
-                self.button_bag = element.texte(18,"Bag", element.black, 760, 650)
+                self.button_bag = element.texte(18,"Bag", element.black, 765, 650)
             if self.action ==4 :
-                self.button_pokedex = element.texte(18,"Pokedex", element.white, 940, 650)
+                self.button_pokedex = element.texte(19,"Pokedex", element.white, 945, 650)
                 element.img(855, 650, 15, 15, f"combat/arrow")
             else:
-                self.button_pokedex = element.texte(18,"Pokedex", element.black, 940, 650)
+                self.button_pokedex = element.texte(18,"Pokedex", element.black, 945, 650)
 
             screen.update()
             
