191cb3e53787492a74c24a676319a49851dd032b
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-01-22 15:26:45 +0100

    changement sur la class experience

--- files/class_py/experience.py
+++ files/class_py/experience.py
@@ -1,42 +1,42 @@
 class Experience:
     def __init__(self, poke_player):
         self.poke_player = poke_player
-        self.exp_winner = 0
-        self.exp_poke = 0
-        self.barre_expMax = 0
+        self.exp_poke = 0       
         self.levels = 1 
         
-    def exp_par_combat(self, evolve):    
-        if evolve == 1:        
-            self.exp_winner =+ 15
-            return self.exp_winner
-        elif evolve == 2:
-            self.exp_winner =+ 30
-            return self.exp_winner
-        elif evolve == 3:
-            self.exp_winner =+ 45
-            return self.exp_winner        
-    
-    def exp_needed(self):        
+    def exp_par_combat(self):    
         if 1 <= self.levels <= 2 :
-            self.barre_expMax = 15
-            
+            self.exp_poke =+ 15            
         if 3 < self.levels < 4:
-            self.barre_expMax = 25
-
+            self.exp_poke =+ 25
         if 5 < self.levels < 10:
-            self.barre_expMax = 30(+5/self.levels)
-
+            self.exp_poke =+ 30
         if 11 < self.levels < 20:
-            self.barre_expMax = 60(+5/self.levels)
-
+            self.exp_poke =+ 45
         if 21 < self.levels < 35:
-            self.barre_expMax = 140(+8/self.levels)
-
+            self.exp_poke =+ 75
         if 36 < self.levels < 50:
-            self.barre_expMax = 290(12/self.levels)
-            
-        return self.barre_expMax
+            self.exp_poke =+ 125            
+        return self.exp_poke
+    
+    def verif_exp(self, level):
+        if level == self.exp_max():
+            level =+ 1        
+    
+    def exp_max(self):        
+        if 1 <= self.levels <= 2 :
+            self.exp_poke = 15            
+        if 3 < self.levels < 4:
+            self.exp_poke = 25
+        if 5 < self.levels < 10:
+            self.exp_poke = 30(+5/self.levels)
+        if 11 < self.levels < 20:
+            self.exp_poke = 60(+5/self.levels)
+        if 21 < self.levels < 35:
+            self.exp_poke = 140(+8/self.levels)
+        if 36 < self.levels < 50:
+            self.exp_poke = 290(12/self.levels)            
+        return self.exp_poke
         
 
 
